e9d015ad8d562d3aa7374f78b21873db2b5abc94
petravetsma <lmikon007@gmail.com>
2023-06-23 09:16:14 +0300

    fix svg load bug

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,23 @@
 import styled from 'styled-components';
+import { ReactComponent as RightArrow } from "assets/right-arrow.svg";
 
 const StyledButton = styled.button`
     vertical-align: top;
-    height: 35px;
     margin: 0;
-    padding: 15px;
+    padding: 4px;
     border: none;
     border-radius: 0 6px 6px 0;
     outline: none;
-    background: 50% no-repeat url('./src/assets/right-arrow.svg') var(--secondary);
+    background: 50% no-repeat var(--secondary);
 
     &:focus {
         outline: none;
     }
+`
+
+const StyledArrow = styled(RightArrow)`
+    height: 100%;
+    width: 100%;
 `
 
 type ButtonProps = {
@@ -28,6 +33,8 @@ type ButtonProps = {
 export function Button(props: ButtonProps) {
 
     return (
-        <StyledButton onClick={props.onClick} />
+        <StyledButton onClick={props.onClick}>
+            <StyledArrow />
+        </StyledButton>
     )
 }
